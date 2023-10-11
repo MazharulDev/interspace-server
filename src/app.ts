b7cc409bdad26bdd9b73e9ba8ee24956bb878e80
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-10-11 22:30:59 +0600

    server ready for works

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+// import routers from "./app/routes";
+import httpStatus from "http-status";
+const app: Application = express();
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+
+app.use(cors());
+app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//aplication main route
+// app.use("/api/v1/", routers);
+
+// tesing route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Home route is Working...");
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+// Not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Route not found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "Api not found",
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+// import { errorlogger, logger } from "./shared/logger";
+import { Server } from "http";
+import config from "./config";
+
+process.on("uncaughtException", (error) => {
+  console.log(error); //errorlogger.error
+  process.exit(1);
+});
+
+let server: Server;
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database is connected`); // logger.info
+
+    server = app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`); //logger.info
+    });
+  } catch (error) {
+    console.log("Failed to connect", error); //errorlogger.error
+  }
+
+  process.on("unhandledRejection", (error) => {
+    if (server) {
+      server.close(() => {
+        console.log(error); //errorlogger.error
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+main();
+
+// process.on('SIGTERM', () => {
+//   logger.info('SIGTERM is received');
+//   if (server) {
+//     server.close();
+//   }
+// });
